3e030bb9a95f15e8f80b507491c4b50cea565ea7
Zachary <zslianggm@gmail.com>
2019-04-14 05:33:06 -0400

    feat: Update binary search algo

--- src/SearchAlgo/SearchAlgo.java
+++ src/SearchAlgo/SearchAlgo.java
@@ -3,16 +3,16 @@ package SearchAlgo;
 public class SearchAlgo {
 
     // Without Repeated numbers
-    public static int binarySearchNR(int[] arr, int target) {
+    public static int bSearchNR(int[] arr, int target) {
         // Return the index of target in sorted array, -1 if target not exist
-        return binarySearchNR(arr, 0, arr.length - 1, target);
+        return bSearchNR(arr, 0, arr.length - 1, target);
     }
 
-    public static int binarySearchNR(int[] arr, int start, int end, int target) {
+    public static int bSearchNR(int[] arr, int start, int end, int target) {
         int low = start, high = end;
 
         while (low <= high) {
-            int mid =  low + (high - low) / 2;
+            int mid =  low + ((high - low) >> 1);
 
             if (target > arr[mid]) {
                 low = mid + 1;
@@ -24,18 +24,81 @@ public class SearchAlgo {
         }
 
         return -1;
-        // variety
-        // Return left or right if they ask you to find the insert position
     }
 
-    // With Repeated numbers
-    public static int findLowerBound(int[] arr, int target) {
-        return findLowerBound(arr, 0 , arr.length - 1, target);
+
+    // Find first target with repeated numbers
+    public static int findFirstTarget(int[] arr, int target) {
+        return findFirstTarget(arr, 0 , arr.length - 1, target);
     }
 
-    public static int findLowerBound(int[] arr, int start, int end, int target) {
-        int idx = findLastSmaller(arr, start, end, target);
-        return arr[idx] == target ? idx : -1;
+    public static int findFirstTarget(int[] arr, int start, int end, int target) {
+        int low = start, high = end;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (arr[mid] > target) {
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                // Equal, first of arr || first of repeated target
+                if ((mid == start) || (arr[mid - 1] != target))
+                    return mid;
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int findLastTarget(int[] arr, int target) {
+        return findLastTarget(arr, 0 ,arr.length - 1, target);
+    }
+
+    public static int findLastTarget(int[] arr, int start, int end, int target) {
+        int low = start, high = end;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (arr[mid] > target) {
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                if ((mid == end) || (arr[mid + 1] != target))
+                    return mid;
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+
+    // Find first elem bigger or equal than >= target
+    public static int findFirstBigger(int[] arr, int target) {
+        return findFirstBigger(arr, 0 , arr.length - 1, target);
+    }
+
+    public static int findFirstBigger(int[] arr, int start, int end, int target) {
+        int low = start, high = end;
+
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+
+            if (arr[mid] >= target) {
+                if((mid == start) || (arr[mid - 1] < target))
+                    return mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
     }
 
     public static int findLastSmaller(int[] arr, int target) {
@@ -45,48 +108,19 @@ public class SearchAlgo {
     public static int findLastSmaller(int[] arr, int start, int end, int target) {
         int low = start, high = end;
 
-        while (low + 1 < high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] >= target) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
 
-        if (arr[low] == target) return low;
-        if (arr[high] == target) return high;
-        return arr[low] < target ? low : high;
-    }
-
-    public static int findHigherBound(int[] arr, int target) {
-        return findHigherBound(arr, 0 ,arr.length - 1, target);
-    }
-
-    public static int findHigherBound(int[] arr, int start, int end, int target) {
-        int idx = findFirstBigger(arr, start, end, target);
-        return arr[idx] == target ? idx : -1;
-    }
-
-    public static int findFirstBigger(int[] arr, int target) {
-        return findFirstBigger(arr, 0 , arr.length - 1, target);
-    }
-
-    public static int findFirstBigger(int[] arr, int start, int end, int target) {
-        int low = start, high = end;
-
-        while (low + 1 < high) {
-            int mid = low + (high - low) / 2;
             if (arr[mid] <= target) {
-                low = mid;
+                if ((mid == end) || (arr[mid + 1] > target))
+                    return mid;
+                low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
 
-        if (arr[high] == target) return high;
-        if (arr[low] == target) return low;
-        return arr[high] > target ? high : low;
+        return -1;
     }
 
 }
