c6b291353e1005cb690e21605d4b13ac35796a38
Zachary <zslianggm@gmail.com>
2019-04-14 05:33:24 -0400

    feat: Update sort algo

--- src/SortAlgo/SortAlgo.java
+++ src/SortAlgo/SortAlgo.java
@@ -1,36 +1,151 @@
 package SortAlgo;
 
+import java.util.Random;
+
 public class SortAlgo {
 
     public static int[] bubbleSort(int[] arr) {
-        // In place sort
+        // 1. In place sort
+        // 2. Stability
+        // 3. Best: O(n), Worst: O(n^2)
         int len = arr.length;
+        if (len <= 1) return arr;
+
         for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            boolean swapped = false;
+
+            for (int j = 0; j < len - 1; j++) {
+                if (arr[i] < arr[j]) {
+                    swap(arr, i, j);
+                    swapped = true;
                 }
             }
+
+            if (!swapped) break; // No swap
         }
         return arr;
     }
 
-    public static int[] mergeSort(int[] arr) {
-        return null;
-    }
+    public static int[] insertionSort(int[] arr) {
+        // 1. In place sort
+        // 2. Stability
+        // 3. Best: O(n), Worst: O(n^2)
+        int len = arr.length;
+        if (len <= 1) return arr;
 
-    public static int[] quickSort(int[] arr) {
-        return null;
-    }
-
-    public static int[] insertSort(int[] arr) {
-        return null;
+        for (int i = 1; i < len; i++) {
+            int val = arr[i];
+            int j = i - 1;
+            // Find out where to insert
+            for (; j >= 0; j--) {
+                if (val < arr[j])
+                    arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = val; // Insert
+        }
+        return arr;
     }
 
     public static int[] selectSort(int[] arr) {
+        // 1. In place sort
+        // 2. Not-Stability
+        // 3. Best: O(n), Worst: O(n^2)
+        int len = arr.length;
+        if (len <= 1) return arr;
+
+        for (int i = 0; i < len - 1; i++) {
+            int min = arr[i];
+            int minIdx = i;
+
+            for (int j = i + 1; j < len; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIdx = j;
+                }
+            }
+            // Swap
+            arr[minIdx] = arr[i];
+            arr[i] = min;
+        }
+
+        return arr;
+    }
+
+    public static void mergeSort(int[] arr) {
+        // 1. Not in place sort
+        // 2. Not-Stability
+        // 3. Time-Avg: O(nlogn), Space: O(n)
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = low + ((high - low) >> 1);
+            // DaC
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            // Merge
+            mergeArr(arr, low, mid, high);
+        }
+    }
+
+    public static void mergeArr(int[] arr, int low, int mid, int high) {
+        int[] sortedArr = new int[high - low + 1];
+        int k = 0, idx1 = low, idx2 = mid + 1;
+
+        while (idx1 <= mid && idx2 <= high) {
+            if (arr[idx1] < arr[idx2])
+                sortedArr[k++] = arr[idx1++];
+            else
+                sortedArr[k++] = arr[idx2++];
+        }
+
+        while (idx1 <= mid) {
+            sortedArr[k++] = arr[idx1++];
+        }
+
+        while (idx2 <= high) {
+            sortedArr[k++] = arr[idx2++];
+        }
+
+        for (int i=low, j=0; i<=high; i++, j++) {
+            arr[i] = sortedArr[j];
+        }
+    }
+
+    public static void quickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int divIdx = partition(arr, low, high);
+            quickSort(arr, low, divIdx - 1);
+            quickSort(arr, divIdx + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];  // Find pivot
+        int divIdx = low;       // Divide index
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                swap(arr, divIdx++, j);
+            }
+        }
+        swap(arr, divIdx, high);
+        return divIdx;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int[] bucketSort(int[] arr) {
         return null;
     }
-    
 }
--- src/SortAlgo/TestSort.java
+++ src/SortAlgo/TestSort.java
@@ -2,6 +2,7 @@ package SortAlgo;
 
 import java.util.Arrays;
 
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class TestSort {
@@ -10,14 +11,25 @@ public class TestSort {
         out.println("Test sorting");
 
         int[] data = new int[]{9, 3, 6, 10, 29, 1, 4, 2, 5, 13, 43, 22, 5, 12};
+        out.println("Original:       " + Arrays.toString(data));
 
-        out.println("Bubble Sort: " + Arrays.toString(SortAlgo.bubbleSort(data)));
+        out.println("Bubble Sort:    " + Arrays.toString(
+                        SortAlgo.bubbleSort(Arrays.copyOf(data, data.length))));
 
-        out.println("Merge Sort: " + Arrays.toString(SortAlgo.mergeSort(data)));
+        out.println("Insertion Sort: " + Arrays.toString(
+                SortAlgo.insertionSort(Arrays.copyOf(data, data.length))));
 
-        out.println("Quick Sort: " + Arrays.toString(SortAlgo.quickSort(data)));
+        out.println("Selection Sort: " + Arrays.toString(
+                SortAlgo.selectSort(Arrays.copyOf(data, data.length))));
 
-        out.println("Select Sort: " + Arrays.toString(SortAlgo.selectSort(data)));
+        int[] msArr = Arrays.copyOf(data, data.length);
+        SortAlgo.mergeSort(msArr);
+        out.println("Merge Sort:     " + Arrays.toString(msArr));
+
+        int[] qsArr = Arrays.copyOf(data, data.length);
+        SortAlgo.quickSort(qsArr);
+        out.println("Quick Sort:     " + Arrays.toString(qsArr));
+
     }
 
 }
